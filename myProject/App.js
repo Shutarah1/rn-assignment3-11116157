de80e6fed4e2e0645c69714bbf47c076732c2a79
Shutarah <ahmoomin@st.ug.edu.gh>
2024-06-02 04:42:40 +0000

    This code includes the necessary imports, the categoryImages object containing the image assets.

---
+++ myProject/App.js
@@ -0,0 +1,15 @@
+import { StatusBar } from 'expo-status-bar';
+import { StyleSheet, Text, View } from 'react-native';
+
+import React from 'react';
+import { StyleSheet, Text, View, TextInput, Image, ScrollView, TouchableOpacity } from 'react-native';
+
+const categoryImages = {
+  'coding.png': require('./assets/coding.png'),
+  'Reading.png': require('./assets/Reading.png'),
+  'Dance.png': require('./assets/Dance.png'),
+  'exercise.png': require('./assets/exercise.png'),
+  'Praying.png': require('./assets/Praying.png'),
+  'jogging.png': require('./assets/jogging.png'),
+  'cooking.png': require('./assets/cooking.png'),
+};
